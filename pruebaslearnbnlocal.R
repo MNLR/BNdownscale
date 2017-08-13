857705bdccb22ab5c6e2ed5e35ab3d3d6e26db6c
MNLR <mlr261@alumnos.unican.es>
2017-08-13 14:11:20 +0200

    Implemented hc.local2(), minor changes
    - Implemented hc.local2(), which does not compute superfluous distances at the expense of:
     -- More complex code and more memory cost.
     -- blacklist asociations are no longer sorted by node. It should be checked if this harms (or improves maybe?) the performance of hc() for big networks.
    
    - function names now reflect the learning algorithm and argument learning.algorithm has been removed.
    
    - Now positions must have each node position  as a column.
    
    - Now compatibility of positions with x is checked.

--- pruebaslearnbnlocal.R
+++ pruebaslearnbnlocal.R
@@ -5,62 +5,29 @@
 
 ##  
 
-positions <- matrix(c(c(2,2), c(3,4)), nrow = 2)
+library(bnlearn)
+
+positions <- matrix(c(c(2,2), c(3,4)), ncol = 2)
 positions
-cosa <- learnbn.local(x = cars, positions, 2)
+cosa <- hc.local2(x = cars, positions, 1)
 cosa
 
-positions <- matrix(c(c(2,2,3,5,10), c(3,4,2,2,10), c(4,4,5,7,10)), nrow = 5)
-out <- learning.complement(nodes.centre = 1,nodes = colnames(iris), positions = positions, distance = 4, norm_ = "2")
-cosa <- learnbn.local(x = iris, positions, 4)
-cosa
-cosa[[1]][1]
-cosa[[1]][2]
+positions <- matrix(c(c(2,3), c(3,3), c(4,5), c(10,10), c(20,35)), ncol = 5, dimnames = list(NULL, colnames(iris)) )
+positions
+learning.complement(nodes.centre = 1,nodes = colnames(iris), positions = positions, distance = 4, norm_ = "2")
 
 
-
-a  matrix(colnames(iris), nrow = 1)
-thing <- matrix(colnames(iris), nrow = 1)
-a <- rbind(a, thing)
-
-x <- iris
-a <- list()
-names <- colnames(x)
-for (i in 1:(NCOL(x)-1)) {
-  
-  a[[i]] <- names
-  names <- names[2:length(names)]
-}
+positions <- matrix(c(c(2,3), c(5,10), c(3,4), c(2,2), c(3,4)), ncol = 5)
+positions
+cosa1 <- hc.local(x = iris, positions, 5)
+cosa1
+cosa2 <- hc.local2(x = iris, positions, 0.1)
+plot(cosa2)
 
 
 survey <- read.table("survey.txt", header = TRUE)
 colnames(survey) #hay que poner 6 distancias
-positions <- matrix(c(c(2,2,10,12,13, 20), c(3,4,5,6,7, 20)), ncol = 2)
+positions <- matrix(c(c(2,2,10,12,13, 20), c(3,4,5,6,7, 20)), nrow = 2)
 positions
-learnt.local <- learnbn.local(x = survey, positions, 1000) # = hc(survey)
-plot(learnt.local)
-
-
-
-
-
-
-
-
-
-
-
-
-
-###############
-###############
-###############
-
-a <- list()
-names <- colnames(x)
-for (i in 1:(NCOL(x)-1)) {
-  a[[i]] <- names
-  names <- names[2:length(names)]
-}
-
-for.the.blacklist <- Mapply(FUN =  learning.complement , nodes.centreS[1:length(nodes.centreS)-1] ,  nodes = nodes, positions=positions, distance=distance, norm=norm)
+learnt.local <- hc.local(survey, positions, 10) # = hc(survey)
+learnt.local
