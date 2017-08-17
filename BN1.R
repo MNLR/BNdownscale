b7724cba8625c57b1aa78b772f63ae8651621796
MNLR <mlr261@alumnos.unican.es>
2017-08-17 11:40:09 +0200

    Added score and time comparison for hc.local() in BN1.R, minor fix in plot.restrictedgraph()
    - BN1.R tests scores of learnt DAGS and benchmarks hc.local() with diferent distances and with hc().
    
    -plot.graphrestrictions() now doesn't display an error message when no distance is specified and plots only nodes and arcs, as if node = -1 had been specified.

--- BN1.R
+++ BN1.R
@@ -3,6 +3,7 @@ library(loadeR.ECOMS)
 library(bnlearn)
 library(igraph)
 library(shape)
+library(microbenchmark)
 
 source("plot.graph.functions.R")
 source("hc.local.R")
@@ -21,8 +22,40 @@ dat$Data[dat$Data >= 1] <-  1
 
 list.tp <- preprocess(dat)
 
-learnt.tp <- hc.local(x = list.tp[[1]], positions = list.tp[[2]], distance = 7, plotrestrictions = T)
-plot.restrictedgraph(learnt.tp, positions = list.tp[[2]], distance = 7, node =10)
-plot.restrictedgraph(learnt.tp, positions = list.tp[[2]], distance = 7, node =-1)
+learnt.tp7 <- hc.local(x = list.tp[[1]], positions = list.tp[[2]], distance = 7, plotrestrictions = T)
+plot.restrictedgraph(learnt.tp7, positions = list.tp[[2]], distance = 7, node =10)
+plot.restrictedgraph(learnt.tp7, positions = list.tp[[2]], distance = 7, node =-1) # plots only nodes and arcs , fixed positions
 
-plot(learnt.tp)
+#plot(learnt.tp)
+
+learnt.tp <- hc(x = list.tp[[1]])  # original hc, no restrictions
+plot.restrictedgraph(learnt.tp, positions = list.tp[[2]])
+
+
+learnt.tp10 <- hc.local(x = list.tp[[1]], positions = list.tp[[2]], distance = 10, plotrestrictions = T)
+plot.restrictedgraph(learnt.tp10, positions = list.tp[[2]], distance = 10, node =10)
+
+learnt.tp5 <- hc.local(x = list.tp[[1]], positions = list.tp[[2]], distance = 5, plotrestrictions = T)
+plot.restrictedgraph(learnt.tp5, positions = list.tp[[2]], distance = 5, node =10)
+
+learnt.tp2 <- hc.local(x = list.tp[[1]], positions = list.tp[[2]], distance = 2, plotrestrictions = T)
+plot.restrictedgraph(learnt.tp2, positions = list.tp[[2]], distance = 2, node =10)
+
+scoreS <- c(score(learnt.tp, list.tp[[1]]),score(learnt.tp10, list.tp[[1]]),score(learnt.tp7, list.tp[[1]]), score(learnt.tp5, list.tp[[1]]), score(learnt.tp2, list.tp[[1]]) )
+
+
+bench <- array(0, 5)
+distanceS <- c(10,7,5,2)
+bench[1] <-   mean(microbenchmark(hc(x = list.tp[[1]]), times = 25)$time)
+for (i in seq(2,5)){
+  bench[i] <-   mean(microbenchmark(hc.local(x = list.tp[[1]], positions =list.tp[[2]], distance = distanceS[i-1]), times = 25)$time)
+}
+
+plot(scoreS)
+plot(bench/(10^9), pch =10)
+
+scoreS
+#[1] -33415.78 -33470.44 -33645.73 -33861.98 -35048.10
+bench/(10^9)
+#[1] 2.9896195 2.5970189 1.6455378 0.8825255 0.1424117 (seg)
+
--- plot.graph.functions.R
+++ plot.graph.functions.R
@@ -25,13 +25,13 @@ plot.graphrestrictions <- function(nodes, positions, distance ) {
 }
 
 
-plot.restrictedgraph <- function(graph, positions, distance, nodes = 0) {
+plot.restrictedgraph <- function(graph, positions, distance = -1, nodes = 0) {
   # Plots the graph of class bn with nodes in positions and shows the nodes dependance distance as a circle, for a given distance d assumed to be euclidean distance. 
   #  ---- INPUT:
   # graph             An object of class bn whose Directed Acyclic Graph is going to be plotted.
-  # positions         sorted array of the locations of x, can only be 1 or 2 dimensional, must contain the same number of columns as the number of variables in x, and number of rows is the dimension.
+  # positions         Sorted array of the locations of x, can only be 1 or 2 dimensional, must contain the same number of columns as the number of variables in x, and number of rows is the dimension.
   #                   Won't check column names, positions must be sorted the same way as the columns (Variables) of x, the data.frame used to learn graph.
-  # distance          maximum distance of dependancy
+  # distance          Maximum distance of dependancy. If no distance is given, no circle will be drawn (so nodes argument will be ignored)
   # nodes             Index of nodes whose dependancy is going to be shown, can be a vector for several nodes. By default, nodes = 0 plots circles for all nodes. -1 will plot no circle, still
   #                   plotting nodes in given positions.
   
@@ -60,7 +60,7 @@ plot.restrictedgraph <- function(graph, positions, distance, nodes = 0) {
   
   color=c("red", "blue", "green", "yellow", "brown", "black", "pink", "cyan")
   plot.igraph(a, layout=t(positions), vertex.size=4, vertex.color=color,  rescale=F,  xlim=c(minx, maxx), ylim=c(miny, maxy), asp=FALSE , axes = TRUE)
-  if (nodes != -1) {
+  if (nodes != -1 & distance != -1) {
     trash <- mapply(plotellipse, mid = split(cpositions, rep(1:ncol(cpositions), each = nrow(cpositions))), lcol = color , MoreArgs = list( rx = distance, ry = distance, asp = FALSE))
   }
 }
