4d4c06e0389bfc1c732b9afcc9c5b4807c18da7f
MNLR <mlr261@alumnos.unican.es>
2017-09-10 19:26:05 +0200

    ++

--- VALUE.GER.R
+++ VALUE.GER.R
@@ -31,7 +31,7 @@ local$Data[ local$Data <= 1] <-  0
 local$Data[ local$Data > 1 ] <-  1
 
 test <- subsetGrid(global, years = 1995)
-real <- subsetGrid(local, years = 1995 )
+real <- subsetGrid(local,  years = 1995)
 
 #from <- array("G.Atmosphere", 53)
 #to <-  c("D.3987", "D.47", "D.2760", "D.2761", "D.4297", "D.51", "D.4472", "D.4669", "D.4079", "D.52", "D.4572", "D.4007", "D.3991", "D.4882", "D.4074", "D.4187", "D.4617",  "D.4954", "D.4014", "D.4776", "D.477", "D.55", "D.4499", "D.4710", "D.42", "D.4652", "D.480", "D.4004", "D.4838", "D.812", "D.4637", "D.475", "D.4284", "D.4218", "D.356", "D.4009", "D.4644", "D.4083",   "D.54", "D.4676",  "D.48", "D.4002", "D.488", "D.2006", "D.472", "D.4015", "D.470", "D.468", "D.3994", "D.58", "D.483", "D.49", "D.469")
@@ -39,7 +39,7 @@ real <- subsetGrid(local, years = 1995 )
 DBN <- build.downscalingBN(local, global , mode = 2, bnlearning.algorithm = hc.local2, 
                            parallelize = TRUE, n.cores = 7,
                            output.marginals = FALSE, 
-                           clustering.args.list = list(k = 4, family = kccaFamily("kmedians") ), 
+                           clustering.args.list = list(k = 4, family = kccaFamily("kmeans") ), 
                            bnlearning.args.list = list(distance = 2.5),
                            param.learning.method = "bayes")
 plot.DBN( DBN, dev=TRUE , nodes = c(28))
